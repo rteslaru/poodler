98c4cfcbfb81ca3f3915829b9f87d9ac1b9f7598
Razvan Teslaru <razvan.teslaru@gmail.com>
2014-04-14 09:47:24 +0200

    initializing repository

---
+++ client/main.js
@@ -0,0 +1,37 @@
+Template.mainTemplate.myVariables = function() {
+    return Clienti.find();
+};
+
+Template.mainTemplate.isDanger = function() {
+    return Meteor.status().connected ? 'btn-danger' : 'btn-primary';
+};
+
+Template.mainTemplate.dangerText = function() {
+    return Meteor.status().connected ? 'disconnect' : 'reconnect';
+};
+
+Template.mainTemplate.events({
+    'click .btn-add': function(e) {
+        e.preventDefault();
+        Clienti.insert({numar: 1});
+    },    
+    'click .btn-disconnect': function(e) {
+        e.preventDefault();
+        if (Meteor.status().connected) {
+            Meteor.disconnect();
+        } else {
+            Meteor.reconnect();
+        }
+    }
+});
+
+Template.detailTemplate.events({
+    'click .btn-decrease': function(e) {
+        e.preventDefault();
+        Clienti.update({_id: this._id}, {$inc: {numar: -1}});
+    },
+    'click .btn-increase': function(e) {
+        e.preventDefault();
+        Clienti.update({_id: this._id}, {$inc: {numar: 1}});
+    }
+});
---
+++ collections/clienti.js
@@ -0,0 +1,1 @@
+Clienti = new Meteor.Collection('clienti');
