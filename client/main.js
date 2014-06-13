db65415ff5d6bf0216b89b022a14714818bc4781
Razvan Teslaru <razvan.teslaru@gmail.com>
2014-06-13 22:34:34 +0200

    adding pageChange event listener -- currently only works forward

--- client/main.js
+++ client/main.js
@@ -7,10 +7,9 @@ var Easing = require('famous/transitions/Easing');
 a = new Transitionable(0);
 b = new Transitionable(0);
 
-Template.famousInit.rendered = function() {
+UI.body.rendered = function() {
     famousCmp.mainCtx.setPerspective(1000);   
 };
-
 
 a.set(1, {duration: 1000, curve: Easing.outElastic});
 b.set(1, {duration: 1000, curve: Easing.outBounce});
@@ -28,12 +27,53 @@ famousCmp.transitions['slideUpSlow'] = {
     }
 };
 
-// famousCmp.registerView('GridLayout', require('famous/views/GridLayout'));
+famousCmp.registerView('Lightbox', require('famous/views/Lightbox'));
+famousCmp.registerView('GridLayout', require('famous/views/GridLayout'));
 // famousCmp.registerView('EdgeSwapper', require('famous/views/EdgeSwapper'));
 
 require('famous/inputs/FastClick');
 
 Transitionable.registerMethod('spring', SpringTransition);
+
+var queue = [];
+
+Meteor.setInterval(function() {
+    if (queue.length)
+        queue.shift()();
+}, 100);
+
+var animateMenu = function(tpl) {
+    var modifier = famousCmp.dataFromTemplate(tpl).modifier;
+ 
+    modifier.setTransform(Transform.translate(0,100,0));
+ 
+    queue.push(function() {
+        modifier.setTransform(
+            Transform.translate(0,0),
+            { duration : 800, curve: 'easeInOut' }
+      );
+    });
+
+};
+
+
+
+Template.homeBtn.rendered = function() {
+    animateMenu(this);
+}
+
+Template.detailTemplate.rendered = function() {
+    animateMenu(this);
+};
+Template.calendarBtn.rendered = function() {
+    animateMenu(this);
+}
+Template.hornBtn.rendered = function() {
+    animateMenu(this);
+}
+Template.userBtn.rendered = function() {
+    animateMenu(this);
+}
 
 UI.body.showContent = function() {
     return Template[Session.get('tpl')];
@@ -44,27 +84,38 @@ var queue = [];
 Session.setDefault('tpl', 'page2');
 Session.setDefault('openSidebar', false);
 
-Template.mainTemplate.myVariables = function() {
+Template.mainScrollview.myVariables = function() {
     return Clienti.find();
 };
 
-Template.mainTemplate.isDanger = function() {
+Template.mainScrollview.isDanger = function() {
     return Meteor.status().connected ? 'btn-danger' : 'btn-primary';
 };
 
-Template.mainTemplate.dangerText = function() {
+Template.mainScrollview.dangerText = function() {
     return Meteor.status().connected ? 'disconnect' : 'reconnect';
 };
 
-Template.mainTemplate.events({
+Template.feed.rendered = function() {
+    famousCmp.dataFromTpl(this).parent.parent.viewNode.on('pageChange', function() {
+        alert('Going to page 2');
+    });
+};
+
+Template.insertBtn.rendered = function() {
+};
+
+Template.insertBtn.events({
     'click .btn-add': function(e) {
         e.preventDefault();
+        alert('asl');
         Clienti.insert({
             numar: 1
         });
     },
     'click .btn-disconnect': function(e) {
         e.preventDefault();
+        alert('asl2');
         if (Meteor.status().connected) {
             Meteor.disconnect();
         } else {
@@ -101,16 +152,12 @@ Template.detailTemplate.events({
     }
 });
 
-Template.sidebar.rendered = function() {
-    // sidebar.setTransform(Transform.translate(0,0,0));    
-};
-
 var toggleSidebar = function() {
     var sidebar = famousCmp.dataFromElement($('.side')[0]).modifier;
-    var menu = famousCmp.dataFromElement($('.main.menu')[0]).modifier;
-    var bottomMenu = famousCmp.dataFromElement($('.bottom.menu')[0]).modifier;
+    // var menu = famousCmp.dataFromElement($('.main.menu')[0]).modifier;
+    // var bottomMenu = famousCmp.dataFromElement($('.bottom.menu')[0]).modifier;
     var openState = Session.get('openSidebar');
-    var body = famousCmp.dataFromElement($('.btn-add')[0]).parent.parent.parent.parent.modifier;
+    var body = famousCmp.dataFromElement($('.list.icon')[0]).parent.parent.modifier;
 
     var transition = {
         duration: 300,
@@ -134,10 +181,6 @@ var toggleSidebar = function() {
         body.setTransform(Transform.translate(0, 0, 0), transition);
         Session.set('openSidebar', false);
     };
-};
-
-Template.mainTemplate.rendered = function() {
-
 };
 
 Template.menu.events({
